c893629641fd76114dcc3eba3e3b7e309e71b970
ShubuYeole <shubhamyeole9@gmail.com>
2024-02-01 11:22:57 +0530

    stack issue #19

---
+++ stack.py
@@ -0,0 +1,33 @@
+class MyStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            raise IndexError("pop from an empty stack")
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            raise IndexError("peek from an empty stack")
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+# Example usage:
+stack = MyStack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print("Size:", stack.size())  
+print("Peek:", stack.peek())  
+print("Pop:", stack.pop())    
+print("Is Empty:", stack.is_empty())  
